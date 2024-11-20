4ccbbadcd0a5c2b937c705ebdc233a7e40129434
Andrey-8 <ademirus1@gmail.com>
2024-11-20 17:52:57 +0500

    Update module_2_2.py

--- modul_2/module_2_2.py
+++ modul_2/module_2_2.py
@@ -1,19 +1,23 @@
-first = 123
-second = 456
-third = 789
-if first == second == third:
-    print(3)
-elif first == second or second == third or first == third:
-    print(2)
-elif first != second != third:
-    print(0)
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
 
-first = 42
-second = 69
-third = 42
-if first == second == third:
+if first != second and second != third and first != third:
+    print(0)
+elif first == second == third:
     print(3)
 elif first == second or second == third or first == third:
     print(2)
-elif first != second != third:
+
+print('ещё разок!')
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+
+if first != second and second != third and first != third:
     print(0)
+elif first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
